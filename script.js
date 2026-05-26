806faedbafc2518a0656202da6b58cb800a843f6
MAYANK PANDEY <101644584+mayank2021264@users.noreply.github.com>
2026-05-26 18:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// complete this js code
 class Person {
-	constructor(name, age){
-		this.name = name;
-		this.age = age;
-	}
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
 
-	greet() {
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-	}
+  greet() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  }
 }
 
 class Employee extends Person {
-	constructor(name, age, jobTitle){
-		super(name, age);
-		this.jobTitle = jobTitle;
-	}
+  constructor(name, age, jobTitle) {
+    super(name, age);
+    this.jobTitle = jobTitle;
+  }
 
-	jobGreet() {
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
-	}
+  jobGreet() {
+    console.log(
+      `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+  }
 }
 
 // Do not change code below this line
